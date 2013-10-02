76005a3c3fd5f70a77edd712602f719f40758ab7
cedric.desire <cedric.desire@gmail.com>
2013-10-02 17:42:00 +0200

    Ajout de la couche service

---
+++ wikommune-services/src/test/java/fr/egloo/wikommune/services/JSoupExampleTest.java
@@ -0,0 +1,25 @@
+package fr.egloo.wikommune.services;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import org.junit.Test;
+
+public class JSoupExampleTest {
+
+	@Test
+	public void testGetDocumentElements() {
+		Document doc;
+		try {
+			doc = Jsoup.connect("http://en.wikipedia.org/").get();
+			Elements newsHeadlines = doc.select("#mp-itn b a");
+			assertNotNull(newsHeadlines);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}	
+}
