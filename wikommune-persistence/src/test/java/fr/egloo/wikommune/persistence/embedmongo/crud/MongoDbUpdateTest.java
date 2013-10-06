c8f942df5fb4a00ba0d6f8e91d6e452bffa7d967
cedric.desire <cedric.desire@gmail.com>
2013-10-06 13:25:25 +0200

    Mise à jour des tests mongo update

--- wikommune-persistence/src/test/java/fr/egloo/wikommune/persistence/embedmongo/crud/MongoDbUpdateTest.java
+++ wikommune-persistence/src/test/java/fr/egloo/wikommune/persistence/embedmongo/crud/MongoDbUpdateTest.java
@@ -1,5 +1,7 @@
 package fr.egloo.wikommune.persistence.embedmongo.crud;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 import com.mongodb.BasicDBObject;
@@ -25,6 +27,15 @@ public class MongoDbUpdateTest extends EmbedMongo {
 	/** Nom de la table servant pour les tests. */
 	private static final String TABLE_NAME = "testCollection";
 
+	/** Constante valeur test. */
+	private static final int VALEUR_100 = 100;
+
+	/** Constante valeur test. */
+	private static final int VALEUR_110 = 110;
+
+	/** Constante valeur test. */
+	private static final int VALEUR_10 = 10;
+
 	/** Objet représentant la table de test. */
 	private DBCollection dbCollection;
 
@@ -47,21 +58,52 @@ public class MongoDbUpdateTest extends EmbedMongo {
 	}
 
 	/**
-	 * Test de mise à jour MongoDB avec $set.
+	 * Test de mise à jour MongoDB avec $set. Mise à jour du document
+	 * "hosting:hostB" en remplaçant la valeur clients:100 par clients:110.
 	 */
 	@Test
 	public final void testUpdateSet() {
+
 		initCollection();
-		// TODO
+
+		BasicDBObject newDocument = new BasicDBObject().append("$set",
+				new BasicDBObject().append("clients", VALEUR_110));
+
+		BasicDBObject searchQuery = new BasicDBObject().append("hosting",
+				"hostB");
+
+		assertEquals(Integer.valueOf(VALEUR_100),
+				dbCollection.findOne(searchQuery).get("clients"));
+
+		dbCollection.update(searchQuery, newDocument);
+
+		assertEquals(Integer.valueOf(VALEUR_110),
+				dbCollection.findOne(searchQuery).get("clients"));
 	}
 
 	/**
-	 * Test de mise à jour MongoDB avec $inc.
+	 * Test de mise à jour MongoDB avec $inc. Mise à jour du document
+	 * "hosting:hostB" en ajoutant 10 à la valeur clients:100 ; clients:100 -->
+	 * clients:110
 	 */
 	@Test
 	public final void testUpdateInc() {
+
 		initCollection();
-		// TODO
+
+		BasicDBObject newDocument = new BasicDBObject().append("$inc",
+				new BasicDBObject().append("clients", VALEUR_10));
+
+		BasicDBObject searchQuery = new BasicDBObject().append("hosting",
+				"hostB");
+
+		assertEquals(Integer.valueOf(VALEUR_100),
+				dbCollection.findOne(searchQuery).get("clients"));
+
+		dbCollection.update(searchQuery, newDocument);
+
+		assertEquals(Integer.valueOf(VALEUR_110),
+				dbCollection.findOne(searchQuery).get("clients"));
 	}
 
 	/**
